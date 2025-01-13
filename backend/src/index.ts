53bd4661d9c74e116634737e31fb83d73bcc68c2
AkshayVoora <145536033+AkshayVoora@users.noreply.github.com>
2025-01-13 09:57:46 -0500

    1st Route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono';
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.('sdc')
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
 })
 
 app.post('/api/v1/user/signin', (c) => {
