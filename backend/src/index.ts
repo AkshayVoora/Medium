e7e762707e32c6a332491e78ed5f2eac81e67654
AkshayVoora <145536033+AkshayVoora@users.noreply.github.com>
2025-01-14 09:19:12 -0500

    Changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono';
 import { sign } from 'hono/jwt'
 
+
 const app = new Hono<{
 	Bindings: {
 		DATABASE_URL: string,
@@ -37,20 +38,19 @@ app.post('/api/v1/user/signin', async (c) => {
 		datasourceUrl: c.env?.DATABASE_URL	,
 	}).$extends(withAccelerate());
 
-  const body = await c.req.json();
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email : body.email
-    }
-  })
-
-  if (!user) {
+	if (!user) {
 		c.status(403);
 		return c.json({ error: "user not found" });
 	}
 
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
 	return c.json({ jwt });
 })
 
