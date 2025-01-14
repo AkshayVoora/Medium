6841ad0791377ff68ae8a4d6c2fadc57d8489e3d
AkshayVoora <145536033+AkshayVoora@users.noreply.github.com>
2025-01-14 14:20:37 -0500

    Added

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -5,21 +5,92 @@ import { sign } from 'hono/jwt'
 
 export const blogRouter = new Hono<{
     Bindings : {
-
+        DATABASE_URL : string
     }
 }>()
 
-blogRouter.post('/blog', (c) => {
-    return c.text('sdc')
+blogRouter.post('/blog', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const blog = await prisma.blog.create({
+		data: {
+			title: body.title,
+			content: body.content,
+            authorId: "1"
+		}
+	});
+
+    return c.json({
+        id: blog.id
+    })
   })
   
-blogRouter.put('/blog', (c) => {
-    return c.text('sdc')
+blogRouter.put('/blog', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const blog = await prisma.blog.update({
+        where:{
+            id : body.id
+        },
+		data: {
+			title: body.title,
+			content: body.content,
+		}
+	});
+
+    return c.json({
+        id: blog.id
+    })
   })
   
-blogRouter.get('/blog/:id', (c) => {
-    return c.text('sdc')
+blogRouter.get('/blog/:id', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+
+    try {
+        const blog = await prisma.blog.findFirst({
+            where:{
+                id : body.id
+            }
+        });
+    
+        return c.json({
+            blog
+        })
+    }catch(e){
+        c.status(411);
+        return c.json({
+            message : "Error while fetching the post"
+        })
+    }
   })
-blogRouter.get('/blog/bulk', (c) => {
-    return c.text('sdc')
+
+blogRouter.get('/blog/bulk', async (c) => {
+    const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+
+    try {
+        const blog = await prisma.blog.findMany();
+    
+        return c.json({
+            blog
+        })
+    }catch(e){
+        c.status(411);
+        return c.json({
+            message : "Error while fetching the post"
+        })
+    }   
   })
